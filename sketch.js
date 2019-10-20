cda317235ff193cbbbb17f6510cae0d9841817a2
Sus-xl <55488310+Sus-xl@users.noreply.github.com>
2019-10-20 15:11:19 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,52 @@
+var colorList = ['#176FA6', '#F29A2E', '#F24957', '#033E8C', '#01402E']
+// var colorList2 = ['brown', 'orange', 'light blue', 'darkgreen', 'yellow']
+
+var mySong;
+var analyzer;
+
 function preload(){
-  // put preload code here
+  mySong = loadSound("./sounds/Borrtex_-_12_-_Merry_Christmas.mp3");
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+
+  // The analyzer allows to perform analysis on a sound file
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(mySong);
 }
 
 function draw() {
-  // put drawing code here
+  var volume = 0;
+  if (mouseX > width/2) {
+    background('#F2E2CE');
+    if (mySong.isPlaying() == false) {
+      mySong.play();
+    }
+    // get the volume and remap it to a bigger value
+    volume = analyzer.getLevel();
+    volume = map(volume,0,1,0,height);
+
+    // SHAPE
+var x = random() * windowWidth;
+var y = random() * windowHeight;
+
+var minDiameter = 4;
+var maxDiameter = 30;
+
+var index = Math.round(random()* (colorList.length - 1));
+var index2 = Math.round(random()* (colorList.length - 1));
+
+fill(color(colorList[index]));
+// stroke(color(colorList2[index2]));
+// strokeWeight(0);
+
+console.log(index, index2);
+
+  } else {
+    background('#176FA6');
+    mySong.stop();
+  }
+
+ellipse(x,y,volume+10);
 }
